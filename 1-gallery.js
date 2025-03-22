d4fba83e546f7333ec28be387e0508607c847d97
Timkaro <timkaro@i.ua>
2025-03-22 12:49:34 +0000

    Deploying to gh-pages from @ Timkaro/goit-js-hw-09@d174fa4d6c202b9592b165dd6bdc665445c2d349 🚀

--- 1-gallery.js
+++ 1-gallery.js
@@ -6,5 +6,5 @@ import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{S as o}from"./assets/
             alt="${p.description}"
           />
         </a>
-      </li>`).join("");i.insertAdjacentHTML("beforeend",t);new o(".gallery a",{captions:!0,captionsData:"alt",captionDelay:250});
+      </li>`).join("");i.insertAdjacentHTML("beforeend",t);new o(".gallery a",{captionsData:"alt",captionDelay:250});
 //# sourceMappingURL=1-gallery.js.map
